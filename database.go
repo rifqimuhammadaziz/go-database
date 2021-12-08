1fcd4b069613688604cc57839225a6e8a5ff1de6
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-08 12:36:19 +0700

    add GetConnection function

---
+++ database.go
@@ -0,0 +1,20 @@
+package godatabase
+
+import (
+	"database/sql"
+	"time"
+)
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/db_godatabase")
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(50 * time.Minute)
+
+	return db
+}
