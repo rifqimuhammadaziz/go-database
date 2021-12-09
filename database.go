eb8075cdf1876830ab4f949bd44bf61c9df9b6b9
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-09 10:55:05 +0700

    update database connection to automatic parse time

--- database.go
+++ database.go
@@ -6,7 +6,7 @@ import (
 )
 
 func GetConnection() *sql.DB {
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/db_godatabase")
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/db_godatabase?parseTime=true") // auto parseTime (date, timestamp)
 	if err != nil {
 		panic(err)
 	}
